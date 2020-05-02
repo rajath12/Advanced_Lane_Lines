53236e3e8ca4a8cc46ee4b0a430d491c5a0881ca
rajath12 <rajath.12@gmail.com>
2020-05-01 18:33:41 -0700

    camera calibration done

--- project.py
+++ project.py
@@ -6,33 +6,23 @@ import matplotlib.image as mpimg
 import glob
 import cv2
 import numpy as np
+import pickle
 
-cal_set = []
-for image in glob.glob('./camera_cal/calibration*.jpg'):
-    n = mpimg.imread(image)
-    cal_set.append(n)
+with open('camera_cal.pickle','rb') as input_file:
+    objpoints,imgpoints = pickle.load(input_file)
 
-img = cal_set[4]
-# initialzing
-imgpoints = [] # 2d image world
-objpoints = [] # 3d image world
+test_images = []
+for image in glob.glob('./test_images/*.jpg'):
+    test_images.append(mpimg.imread(image))
 
-objp = np.zeros((6*9,3), np.float32) # size of array is 9 by 6
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2) # x and y coordinates
-
-gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-img_size = (img.shape[1], img.shape[0])
-
-ret,corners = cv2.findChessboardCorners(gray, (9,6), None)
-if ret == True:
-    imgpoints.append(corners)
-    objpoints.append(objp)
-
-    img = cv2.drawChessboardCorners(img,(9,6),corners,ret)
-
+image1 = test_images[6]
+img_size = (image1.shape[1], image1.shape[0])
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size , None, None)
-dst = cv2.undistort(img, mtx, dist, None, mtx)
+dst = cv2.undistort(image1, mtx, dist, None, mtx)
 
+plt.figure(1)
+plt.subplot(121)
+plt.imshow(image1)
+plt.subplot(122)
 plt.imshow(dst)
 plt.show()
