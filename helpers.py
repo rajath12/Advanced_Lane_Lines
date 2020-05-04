90ac1e776b974eb5d4a826c9a8063597c28ab192
rajath12 <rajath.12@gmail.com>
2020-05-04 12:38:21 -0700

    updated

--- helpers.py
+++ helpers.py
@@ -4,29 +4,28 @@ import matplotlib.image as mpimg
 import cv2
 import numpy as np
 
-def birdseye(image):
-    ''' Takes in an undistorted image and returns the unwarped image image.'''    
-    img_size = (image.shape[1],image.shape[0]) # be wary of this variable during implementation
-
+def getTransformMatrices():
     # source points from undistorted image and destination points
+    x = 1280
+    y = 720
     src = np.float32([[556,450],[685,450],[980,650],[295,650]]) # found coordinates using cv2 lines
     offset = 250 
     dest = np.float32([
                     [offset,0],
-                    [image.shape[1]-offset,0],
-                    [image.shape[1]-offset,image.shape[0]],
-                    [offset,image.shape[0]]])
+                    [x-offset,0],
+                    [x-offset,y],
+                    [offset,y]])
     M = cv2.getPerspectiveTransform(src,dest) # perspective matrix
+    Minv = np.linalg.inv(M)
+    return M
+
+def birdseye(image):
+    ''' Takes in an undistorted image and returns the unwarped image image.'''    
+    M = getTransformMatrices()
+    img_size = (image.shape[1],image.shape[0]) # be wary of this variable during implementation
     warped = cv2.warpPerspective(image,M,img_size) # birds-eye view
     # dst = cv2.line(dst,(980,650),(295,650),[255,0,0],thickness = 2) # use for debugging coordinates
     return warped
-    
-def hist(image):
-    '''take in a perspective transformed image and draw the histogram based on 
-    lane lines found near the bottom'''
-    bottom_half = image[(image.shape[0]-image.shape[0]//2):,:]
-    hist = np.sum(bottom_half, axis=0)
-    return hist
     
 def plot_all(folder):
     '''takes in an image folder, do the appropriate stuff and plot all output images'''
